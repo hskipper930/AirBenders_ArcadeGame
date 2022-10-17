77eb6e83e4ef17dfc39d99d65f0b9e1e31f8400b
DungeonTurtle <65980438+DungeonTurtle@users.noreply.github.com>
2022-10-17 15:39:47 -0400

    Trying to look

---
+++ Assets/Scripts/MeleeBehavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeBehavior : MonoBehaviour
+{
+    GameObject owner;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    public void SetOwner()
+    {
+
+    }
+
+    private void get
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,10 +17,12 @@ public class Player : MonoBehaviour
     //----------Combat Parameters--------------
     #region CombatParameters
     [SerializeField] private int HP;
-    [SerializeField] private float Damage;
+    [SerializeField] private int damage;
     private Vector3 mousePos;
     [SerializeField] private GameObject meleeHitbox;
     [SerializeField] private GameObject rangedHitbox;
+    [SerializeField] private float meleeAttackRange;
+    public LayerMask enemyLayer;
     #endregion CombatParameters
 
     // Start is called before the first frame update
@@ -34,29 +36,54 @@ public class Player : MonoBehaviour
     {
         //----------Movement--------------
         movement.x = Input.GetAxisRaw("Horizontal");
-        movement.z = Input.GetAxisRaw("Vertical");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        Ray lookRay = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit look;
+        if (Physics.Raycast(lookRay, out look, 100))
+        {
+            mousePos = look.point;
+        }
+        Debug.Log("Mouse Position: " + mousePos);
 
         if (Input.GetMouseButtonDown(1))
         {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, 100))
-            {
-                mousePos = hit.point;
-            }
-            Debug.Log("Mouse Position: " + mousePos);
-
             GameObject shot = Instantiate(rangedHitbox, transform.position, transform.rotation);
             shot.GetComponent<ProjectileMovement>().SetTarget(mousePos);
         }
 
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //GameObject shot = Instantiate(meleeHitbox, transform.position, transform.rotation);
+            //shot.GetComponent<ProjectileMovement>().SetTarget(mousePos);
+        }
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log("Mouse Position: " + mousePos);
     }
 
     private void FixedUpdate()
     {
         //--------Movement-------------
         playerRB.MovePosition(playerRB.position + movement * movementSpeed * Time.fixedDeltaTime);
+
+        //Vector3 lookDir = mousePos - playerRB.position;
+        //float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        Quaternion lookRot = Quaternion.LookRotation(mousePos, Vector3.forward);
+        playerRB.rotation = lookRot;
+    }
+
+    private void Attack()
+    {
+        float mR = meleeAttackRange;
+        Vector3 meleeRange;
+        Vector3 attackPoint = transform.position + new Vector3(mR, 0, 0);
+        Collider[] hitEnemies = Physics.OverlapBox(attackPoint, meleeRange = (attackPoint + new Vector3(mR / 2, mR / 2, 0)), transform.rotation);
+
+        foreach (var enemy in hitEnemies)
+        {
+            Debug.Log("EnemyHit");
+            enemy.GetComponent<EnemyAI>().TakeDamage(damage);
+        }
     }
 }
